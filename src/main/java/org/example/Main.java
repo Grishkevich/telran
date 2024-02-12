b511522ca06cf3fc7315242bd2c8cd00c0d399d1
gribodemon <gribodemong@gmail.com>
2024-02-12 23:17:53 +0200

    work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello worlasdasdd!");
+        Test x = new Test("aaa", 222);
+
+        System.out.println(x);
     }
 }
 
---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import lombok.*;
+
+
+//@NoArgsConstructor
+@AllArgsConstructor
+public @Data class Test {
+    String x;
+    int a;
+}
