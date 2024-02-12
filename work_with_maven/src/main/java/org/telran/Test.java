49cb4b4b5c03cfe7b3c8baa0ae02b56a8ec7adb2
gribodemon <gribodemong@gmail.com>
2024-02-12 23:23:16 +0200

    work

---
+++ work_with_maven/src/main/java/org/telran/Test.java
@@ -0,0 +1,14 @@
+package org.telran;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+
+//@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public @Data class Test {
+    String x;
+    int a;
+}
